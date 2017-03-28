01d1716bdd9e0adc68f77d5a2e939c0dfb6d7aa2
Nathan Kerr <Nathan.Kerr@nordicsemi.no>
2017-03-28 12:30:50 -0700

    Update version number.

--- test/ddo_test.dart
+++ test/ddo_test.dart
@@ -1,5 +1,5 @@
-
-import 'package:unittest/unittest.dart';
+@TestOn('vm')
+import 'package:test/test.dart';
 import '../lib/ddo.dart';
 import '../lib/drivers/ddo_mysql.dart';
 @MirrorsUsed(
@@ -9,13 +9,13 @@ import '../lib/drivers/ddo_mysql.dart';
 import 'dart:mirrors';
 
 main() {
-	Driver driver = new DDOMySQL('localhost', 'redstone_test', 'redstone', 'password');
+	Driver driver = new DDOMySQL('localhost', 'example', 'root', '');
 	DDO ddo = new DDO(driver);
 	test('Quote correctly', (){
 		expect(ddo.quote(r"''\Nathan"), equals("\'\\'\\'\\\\Nathan\'"));
 		expect(ddo.quote("1"), equals("'1'"));
 	});
 	test('Retrieve from database', (){
-		expect(ddo.query('select * from user'), completion(new isInstanceOf<DDOStatement>()));
+		expect(ddo.query('select * from person'), completion(new isInstanceOf<DDOStatement>()));
 	});
 }
--- test/drivers/ddo_websql.dart
+++ test/drivers/ddo_websql.dart
@@ -1,25 +1,32 @@
-import 'package:unittest/unittest.dart';
+@TestOn('chrome')
+import 'package:test/test.dart';
 import '../../lib/drivers/ddo_websql.dart';
 import '../../lib/ddo.dart';
-import 'package:unittest/html_config.dart';
 
 main() {
-	useHtmlConfiguration();
 	Driver driver = new DDOWebSQL(name: 'ddo_websql_test');
 	DDO ddo = new DDO(driver);
 	group('Database tests:', () {
-		setUp(() => ddo.exec('CREATE TABLE IF NOT EXISTS person ("id" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,"name" TEXT NOT NULL,"created" TEXT NOT NULL)'));
-		tearDown(() => ddo.exec('DROP TABLE person'));
-		test('it should be empty', () {
-			return ddo.query('SELECT count(*) from person').then((DDOStatement stmt) {
-				expect(stmt.fetch(DDO.FETCH_NUM), equals(0));
-			});
+		setUp(() async {
+			await ddo.exec(
+				'CREATE TABLE IF NOT EXISTS person ("id" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,"name" TEXT NOT NULL,"created" TEXT NOT NULL)');
 		});
-		test('it should insert a row', (){
-			expect(ddo.query("INSERT INTO person (name, created) VALUES ('Nathan', '${new DateTime.now().toIso8601String()}')"), completion(new isInstanceOf('DDOStatement')));
-			return ddo.query('SELECT count(*) from person').then((DDOStatement stmt) {
-				expect(stmt.fetch(DDO.FETCH_NUM), equals(1));
-			});
+
+		tearDown(() async {
+			await ddo.exec('DROP TABLE person');
+		});
+
+		test('it should be empty', () async {
+			DDOStatement stmt = await ddo.query('SELECT count(*) from person');
+			expect(stmt.fetch(DDO.FETCH_NUM), equals(0));
+
+		});
+
+		test('it should insert a row', () async {
+			DDOStatement stmt = await ddo.query("INSERT INTO person (name, created) VALUES ('Nathan', '${new DateTime.now().toIso8601String()}')");
+			expect(stmt, new isInstanceOf<DDOStatement>());
+			stmt = await ddo.query('SELECT count(*) from person');
+			expect(stmt.fetch(DDO.FETCH_NUM), equals(1));
 		});
 
 	});
